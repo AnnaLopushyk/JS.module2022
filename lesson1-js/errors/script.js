310cd212aba1245e2b2a97aebe92251b895291ee
AnnaLopushyk <anna.lopushyk@gmail.com>
2022-07-22 00:29:34 +0300

    errors

---
+++ lesson1-js/errors/script.js
@@ -0,0 +1,14 @@
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+const x = 'constant value';
+{
+    console.log(a);
+   let x = 'new value';
+}
+ let y = ' new value';
+
+let result = 100 + y;
+
+console.log(result);
